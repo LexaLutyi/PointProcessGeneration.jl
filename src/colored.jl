f5e320048c9f0184063499fd4b752a5dba9674f6
LexaLutyi <liotbiu1@gmail.com>
2022-02-02 17:52:34 +0300

    fixes

--- src/colored.jl
+++ src/colored.jl
@@ -1,7 +1,7 @@
-function normal_clusters(nx, ny; dim=2)
+function normal_clusters(nx, ny; dim=2, σ=0.1)
     x0 = poisson(nx; dim)
     λ = ny / nx
-    y0 = clusters(x0, () -> normal(rand(Poisson(λ)); dim) / λ)
+    y0 = clusters(x0, () -> normal(rand(Poisson(λ)); dim) * σ)
     x0, y0
 end
 
@@ -15,7 +15,7 @@ end
 
 
 function anarchy(n)
-    z = poisson(2, n)
+    z = poisson(n)
     ix = map(w -> w[1] > w[2], eachcol(z))
     x0 = z[:, ix]
     y0 = z[:, .!ix]
@@ -23,14 +23,14 @@ function anarchy(n)
 end
 
 
-function poisson(nx, ny; dim)
+function poisson(nx, ny; dim=2)
     x0 = poisson(nx; dim)
     y0 = poisson(ny; dim)
     x0, y0
 end
 
 
-function colored_clusters(nsup, npp)
+function colored_clusters(nsup, npp; σ=0.1)
     dim = 2
     z = poisson(4nsup; dim)
     d = pairwise(PeriodicEuclidean(ones(dim)), z; dims=2)
@@ -40,18 +40,18 @@ function colored_clusters(nsup, npp)
     xs = z[:, 1:2:end]
     ys = z[:, 2:2:end]
     λ = npp / nsup
-    x0 = clusters(xs, () -> normal(rand(Poisson(λ)); dim) / λ)
-    y0 = clusters(ys, () -> normal(rand(Poisson(λ)); dim) / λ)
+    x0 = clusters(xs, () -> normal(rand(Poisson(λ)); dim) * σ)
+    y0 = clusters(ys, () -> normal(rand(Poisson(λ)); dim) * σ)
     x0, y0
 end
 
 
 function target_circles(n=1500, radius=0.125)
     dim = 2
-    z = poisson(dim, n)
+    z = poisson(n; dim)
     center = reshape([0.5, 0.5], 2, 1)
     d = pairwise(PeriodicEuclidean(ones(2)), z, center; dims=dim)[:]
-    ix = mod.(div.(d, r), 2) .== 1
+    ix = mod.(div.(d, radius), 2) .== 1
     x0 = z[:, ix]
     y0 = z[:, .!ix]
     x0, y0
